a0b5396cb16d633784edb62055e41dfcfa1b0770
Roshan Dalvi <roshandalvi16@gmail.com>
2025-08-04 21:48:52 +0530

    2nd Way using boolean ispalindrome = true

--- DSA_Hackerrank/Java_String_Reverse_palindrome.java
+++ DSA_Hackerrank/Java_String_Reverse_palindrome.java
@@ -32,3 +32,37 @@ public class Java_String_Reverse_palindrome {
 
 
 
+
+
+
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+        
+//         Scanner sc=new Scanner(System.in);
+//         String A=sc.next();
+//         boolean ispalindrome = true;
+        
+//         for (int i = 0; i < A.length()/2; i++) {
+//             if(A.charAt(i) != A.charAt(A.length()-1-i)){
+//                 ispalindrome = false;
+//                 break;
+//             }
+//         }  
+        
+//         if(ispalindrome){
+//             System.out.print("Yes");
+//         }else{
+//             System.out.print("No");
+//         }
+        
+        
+//     }
+// }
+
+
+
